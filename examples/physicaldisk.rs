fd16ecf284942bd81a1c0d3d43f2ef3d540a382c
莯凛 <mokurin000@gmail.com>
2025-11-08 06:45:20 +0800

    docs: add example and docs for PhysicalDisk

---
+++ examples/physicaldisk.rs
@@ -0,0 +1,22 @@
+use std::{env, error::Error};
+
+use listdisk_rs::win32::{drive_info::diskindex_by_driveletter, physical_disk::PhysicalDisk};
+use wmi::WMIConnection;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let wmi_storage = WMIConnection::with_namespace_path(r#"ROOT\Microsoft\Windows\Storage"#)?;
+
+    let physical_disks = wmi_storage.query::<PhysicalDisk>()?;
+
+    let systemdrive = env::var("SystemDrive").unwrap_or("C:".into());
+    let diskindex = diskindex_by_driveletter(systemdrive.chars().next().unwrap())?.to_string();
+
+    let physical_system_drive = physical_disks
+        .into_iter()
+        .find(|d| d.device_id == diskindex)
+        .expect("cannot find matched device");
+
+    println!("System drive info:\n{physical_system_drive:#?}");
+
+    Ok(())
+}
--- src/win32/physical_disk/mod.rs
+++ src/win32/physical_disk/mod.rs
@@ -3,19 +3,18 @@ use serde::{Deserialize, Serialize};
 #[cfg(feature = "serde_repr")]
 mod typing;
 #[cfg(feature = "serde_repr")]
-pub use typing::{BusType, Usage, HealthStatus, MediaType};
-
+pub use typing::{BusType, HealthStatus, MediaType, Usage};
 
 /// ```rust
 /// use wmi::WMIConnection;
 /// use listdisk_rs::win32::physical_disk::PhysicalDisk;
 ///
 /// let wmi_storage = WMIConnection::with_namespace_path(r#"ROOT\Microsoft\Windows\Storage"#)?;
-/// 
+///
 /// let phy_drives = wmi_storage.query::<PhysicalDisk>()?;
 /// Ok::<_, wmi::WMIError>(())
 /// ```
-/// 
+///
 /// Also see [MSDN](https://learn.microsoft.com/en-us/windows-hardware/drivers/storage/msft-physicaldisk)
 #[cfg(feature = "serde")]
 #[derive(Debug, Serialize, Deserialize)]
@@ -67,6 +66,33 @@ pub struct PhysicalDisk {
     pub enclosure_number: Option<u16>,
     pub slot_number: Option<u16>,
     pub can_pool: bool,
+    /// An array of values specifying the reasons why this physical disk cannot be added to a concrete pool. This property is valid only if the CanPool property is FALSE.
+    ///
+    /// Unknown (0)
+    ///
+    /// Other (1)
+    ///
+    /// In a Pool (2)
+    ///
+    /// Not Healthy (3)
+    ///
+    /// Removable Media (4)
+    ///
+    /// In Use by Cluster (5)
+    ///
+    /// Offline (6)
+    ///
+    /// Insufficient Capacity (7)
+    ///
+    /// Spare Disk (8)
+    ///
+    /// Reserved by subsystem (9)
+    ///
+    /// Starting (10)
+    ///
+    /// Microsoft Reserved (..)
+    ///
+    /// Vendor Reserved (0x8000..)
     pub cannot_pool_reason: Vec<u16>,
     pub other_cannot_pool_reason_description: Option<String>,
     pub is_partial: bool,
